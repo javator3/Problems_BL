243dd673be75e7ebec0c09aad68220577149ff42
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-02-17 15:25:41 +0100

    resolved frist problem

--- src/main/java/P01.java
+++ src/main/java/P01.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class P01 {
+
+
+    public static String last(List<String> list) {
+        return list.get(list.size()-1);
+    }
 }
--- src/test/java/Problems.java
+++ src/test/java/Problems.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class Problems {
+
+    @Test
+    public void shouldFindLastElementFromListofAlphabets() throws  Exception{
+        assertThat(P01.last(asList("a", "b", "c", "d")), is(equalTo("d")));
+    }
 }
